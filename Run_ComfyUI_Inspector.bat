cf1b451c74b12e49772f9a09ecb6577835437648
Mounir Belahbib <shinystudioxyz@gmail.com>
2025-08-21 03:46:56 +0200

    Update Run_ComfyUI_Inspector.bat
    BUG FIX : Contained forbidden characters

--- Run_ComfyUI_Inspector.bat
+++ Run_ComfyUI_Inspector.bat
@@ -1,6 +1,7 @@
 @echo off
 cd ..
+if not exist reports mkdir reports
 set datetime=%date:~10,4%-%date:~4,2%-%date:~7,2%_%time:~0,2%-%time:~3,2%
 set datetime=%datetime: =0%
-python ComfyUI-Ultimate-Inspector\ComfyUI-Ultimate-Inspector.py --output report-%datetime%.md --verbose
+python ComfyUI-Ultimate-Inspector\ComfyUI-Ultimate-Inspector.py --output reports\report-%datetime%.md --verbose
 pause
